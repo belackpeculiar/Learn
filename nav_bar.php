c187ca2f6035455ba24ce7edd03c6a6f7e485c44
belackpeculiar <peculiarbelack@gmail.com>
2023-08-09 08:16:35 +0200

    add nav_bar file

---
+++ nav_bar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>nav bar</title>
+</head>
+<body>
+    <div class="top_bar">
+        <span></span>
+        <ul>
+            <li><a href=""></a>Home</li>
+            <li><a href=""></a>Contact</li>
+        </ul>
+    </div>
+</body>
+</html>
